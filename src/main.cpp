475bad83f212483590df83fb5b20d2ee7b126049
BOREKBenoit <borekbenoit8@gmail.com>
2025-03-04 16:15:21 +0100

    ajout du capteur SHT-C3, reste à intégrer le BMP280, l'anémomètre, la communication LoRa et les autres commandes AT.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <Wire.h> //Importation de la bibliothèque Wire pour pouvoir communiquer avec le capteur.
+#define Addr 0x70 //Définition de la variable Addr avec la valeur héxadécimale 0x70.
+
 String data; 
 char sortie[120];
 
@@ -6,22 +9,21 @@ int reveil = 6;
 int verifreveil = 7;
 int reponse = 0;
 
-
+float puissance = pow(2, 16);
 
 void decode();  // Déclaration de la fonction
-
 
 
 void setup() {
 
   pinMode(reveil, OUTPUT);
   pinMode(verifreveil, INPUT);
-  delay(1000);
+  delay(2000);
   if(digitalRead(verifreveil)== HIGH){
     digitalWrite(reveil, LOW);
   } else {
     digitalWrite(reveil, HIGH);
-    delay(1000);
+    delay(2000);
     digitalWrite(reveil, LOW);
   }
   delay(2000);
@@ -46,6 +48,15 @@ void setup() {
   Serial1.println("AT+CPIN=\"0000\"");
   decode();
 
+
+
+  Wire.begin(); //Initialisation de la librairie Wire et de l'I2C.
+
+  Wire.beginTransmission(Addr); //Initialisation de la communication Arduino <----> SHTC3 avec l'adresse 0x70.
+  Wire.write(0x80); //Envoi sur 2 octets la commande de réinitialisation système du capteur.
+  Wire.write(0x5D);
+   Wire.endTransmission();
+
  
 
   
@@ -54,6 +65,56 @@ void setup() {
 }
 
 void loop() {
+
+  Wire.beginTransmission(Addr); //Envoi de la commande de reveil avec le code 0x3517 sur 2 octets.
+  Wire.write(0x35);
+  Wire.write(0x17);
+  Wire.endTransmission();
+  delay(1000);
+  Wire.beginTransmission(Addr); //Envoi de la commande de conversion en "Normal mode" avec la température lu en premier.
+  Wire.write(0x7C); //Envoi de 0x7CA2 sur 2 octets.
+  Wire.write(0xA2);
+  Wire.endTransmission();
+  delay(1000); //Attente d'une seconde pour laisser le temps au capteur de convertir la température et l'humidité.
+
+  Wire.requestFrom(Addr, 6); //Envoi d'une requête de données sur 6 octets.
+  // Deux octets sont pour la température, deux octets pour l'humidité et deux octets pour les checksum.
+  Serial.print("Octets en attente de lecture: "); //Affichage dans le moniteur série du nombre d'octets disponible à intégrer dans des variables.
+  Serial.println(Wire.available());
+  int T_MSB = Wire.read(); //Lecture du premier octet de température.
+  int T_LSB = Wire.read(); //Lecture du deuxième octet de température.
+  int checksum1 = Wire.read(); // Attribution du checksum à une variable checksum1.
+  int H_MSB = Wire.read(); //Lecture du premier octet d'humidité.
+  int H_LSB = Wire.read(); //Lecture du deuxième octet d'humidité.
+  int checksum2 = Wire.read(); // Attribution du checksum à une variable checksum2.
+
+  float Temp = (T_MSB*255) + T_LSB; 
+  Temp = -45 + 175 * (Temp/puissance); 
+
+  //Conversion de l'humidité en valeur décimale.
+  float Hum = (H_MSB*255) + H_LSB;
+  Hum = 100 * (Hum/puissance);
+ // Hum = 100 + Hum;
+
+  Serial.println(); //Affichage de la température et de l'humidité dans le moniteur série.
+  Serial.print("Température convertie : ");
+  Serial.print(Temp);
+  Serial.println("°C");
+  Serial.println();
+  Serial.print("Humidité convertie : ");
+  Serial.print(Hum);
+  Serial.println("%");
+  Serial.println();
+
+
+
+
+  Wire.beginTransmission(Addr); //Envoi sur 2 octets de la commande de mise en veille du capteur.
+  Wire.write(0xB0); //Commande de mise en veille 0xB098.
+  Wire.write(0x98);
+  Wire.endTransmission();
+  delay(1000);
+
 
 }
 
